6c66655082d5a2d7d2b57764d421cdea40044d9f
Mike Morano <mmorano@mikeandwan.us>
2020-06-06 06:59:57 -0400

    add github action to build and push container image

---
+++ _prod_build_and_push.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+IID_FILE=image.iid
+TAG=$(date +%Y%m%d%H%M%S)
+
+buildah bud -f Containerfile.prod -t maw-certs --iidfile "${IID_FILE}"
+IID=$(cat "${IID_FILE}")
+buildah push --creds "${DH_USER}:${DH_PASS}" "${IID}" docker://aerisg222/maw-certs:"${TAG}"
+buildah push --creds "${DH_USER}:${DH_PASS}" "${IID}" docker://aerisg222/maw-certs:latest
